b8280b148708f18b61f51474f934d497f839cd14
carolin randala <carolinrandala@gmail.com>
2022-12-05 18:47:52 +0200

    bug fix

--- src/main/java/org/sda/java19/Main.java
+++ src/main/java/org/sda/java19/Main.java
@@ -19,12 +19,13 @@ import java.util.stream.Collectors;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        /*//Download data from a file and create objects of people based on them (in any way - Regex, String.split ...).
+        //Download data from a file and create objects of people based on them (in any way - Regex, String.split ...).
         Path absolutePath = Paths.get("/Users/carolinrandala/Desktop/Learning-java/java-start-basics/java-advanced-coding/src/main/resources/data.txt");
 
         //Reading file
         List<String> fileLines = Files.readAllLines(absolutePath, StandardCharsets.UTF_8);
         List<Person> dataList = new ArrayList<>();
+
 
 
         for (String fileLine : fileLines) {
@@ -39,38 +40,9 @@ public class Main {
             System.out.println(dataList);
 
 
-
-*/
-
-
-            //Enter the created objects into Array from the file:
-
-            String path = "/Users/carolinrandala/Desktop/Learning-java/java-start-basics/java-advanced-coding/src/main/resources/data.txt";
-            String line = "";
-            int lineCount = 0;
-
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(path));
-                while ((line = br.readLine()) != null) {
-                    String[] values = line.split(",");
-                    lineCount++;
-                    System.out.println(lineCount + " Name: " + values[0] + "\nSurname:" + values[1] + "\nPhonenumber:" + values[2]);
-                }
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-
-           /* dataList.stream()
-                    .sorted(Comparator.comparing(Person::getName))
-                    .collect(Collectors.toList())
-                    .forEach(person1 -> System.out.println(person));*/
-
-
         }
     }
+}
 
 
 
